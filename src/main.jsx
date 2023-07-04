f03a19599e8034c310ae7af9434d18883be00093
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-07-04 23:14:12 +0600

    make popular courses section with cards, added react-rating

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import router from "./routes/router.jsx";
+import '@smastrom/react-rating/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from "../components/Navbar/Navbar.jsx";
 import HeroSection from "../components/HeroSection/HeroSection.jsx";
+import PopularClasses from "../components/PopularClasses/PopularClasses.jsx";
 
 const Home = () => {
     return (
         <div>
             <HeroSection/>
+            <PopularClasses/>
         </div>
     );
 };
