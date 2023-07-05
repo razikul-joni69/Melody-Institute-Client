e333179a8eb795eefc386b2ce110a177523469e6
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-07-05 11:02:52 +0600

    make popular instructors section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from "../components/Navbar/Navbar.jsx";
 import HeroSection from "../components/HeroSection/HeroSection.jsx";
 import PopularClasses from "../components/PopularClasses/PopularClasses.jsx";
+import PopularInstructors from "../components/PopularInstructors/PopularInstructors.jsx";
 
 const Home = () => {
     return (
         <div>
             <HeroSection/>
             <PopularClasses/>
+            <PopularInstructors/>
         </div>
     );
 };
